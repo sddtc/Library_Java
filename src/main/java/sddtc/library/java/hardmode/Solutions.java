db94433f004ff2edca38afc0dcbc9070e2c1d058
tuijiang <tuijiang@mogujie.com>
2016-03-24 17:15:48 +0800

    Add no.207

--- src/main/java/sddtc/library/java/hardmode/Solutions.java
+++ src/main/java/sddtc/library/java/hardmode/Solutions.java
@@ -544,4 +544,43 @@ public class Solutions {
 
         return result.length() == 0 ? "/" : result.toString();
     }
+
+    /**
+     * no.207 https://leetcode.com/problems/course-schedule/
+     *
+     * @param numCourses    要完成的课程数
+     * @param prerequisites 条件
+     * @return 能不能完成课程呢
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> posts = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            posts.add(new ArrayList<Integer>());
+        }
+        int[] preNums = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            posts.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            preNums[prerequisites[i][0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (preNums[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int count = numCourses;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int i : posts.get(cur)) {
+                if (--preNums[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+            count--;
+        }
+
+        return count == 0;
+    }
 }
