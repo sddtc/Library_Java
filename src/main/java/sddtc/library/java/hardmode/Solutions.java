6ef9aea3b0c1d907209fa2845a960e268280c4a6
changh <changhbaga@gmail.com>
2016-04-30 14:53:54 +0800

    Add 138

--- src/main/java/sddtc/library/java/hardmode/Solutions.java
+++ src/main/java/sddtc/library/java/hardmode/Solutions.java
@@ -1,9 +1,6 @@
 package sddtc.library.java.hardmode;
 
-import sddtc.library.java.object.Interval;
-import sddtc.library.java.object.ListNode;
-import sddtc.library.java.object.Point;
-import sddtc.library.java.object.TreeNode;
+import sddtc.library.java.object.*;
 
 import java.util.*;
 
@@ -896,5 +893,57 @@ public class Solutions {
             digit--;
         }
         return isNeg ? -result : result;
+    }
+
+    /**
+     * no.138 https://leetcode.com/problems/copy-list-with-random-pointer/
+     *
+     * @param head
+     * @return
+     */
+    public RandomListNode copyRandomList(RandomListNode head) {
+        RandomListNode iter = head, next;
+
+        // First round: make copy of each node,
+        // and link them together side-by-side in a single list.
+        while (iter != null) {
+            next = iter.next;
+
+            RandomListNode copy = new RandomListNode(iter.label);
+            iter.next = copy;
+            copy.next = next;
+
+            iter = next;
+        }
+
+        // Second round: assign random pointers for the copy nodes.
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null) {
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+
+        // Third round: restore the original list, and extract the copy list.
+        iter = head;
+        RandomListNode pseudoHead = new RandomListNode(0);
+        RandomListNode copy, copyIter = pseudoHead;
+
+        while (iter != null) {
+            next = iter.next.next;
+
+            // extract the copy
+            copy = iter.next;
+            copyIter.next = copy;
+            copyIter = copy;
+
+            // restore the original list
+            iter.next = next;
+
+            iter = next;
+        }
+
+        return pseudoHead.next;
     }
 }
---
+++ src/main/java/sddtc/library/java/object/RandomListNode.java
@@ -0,0 +1,14 @@
+package sddtc.library.java.object;
+
+/**
+ * Created by sddtc on 16/4/30.
+ */
+public class RandomListNode {
+    public int label;
+    public RandomListNode next;
+    public RandomListNode random;
+
+    public RandomListNode(int x) {
+        this.label = x;
+    }
+}
