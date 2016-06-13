dbfb839960ae7dbf92514e2fcae4c46cd0c4f742
changh <changhbaga@gmail.com>
2016-06-13 08:33:44 +0800

    add

--- src/main/java/sddtc/library/java/hardmode/Solutions.java
+++ src/main/java/sddtc/library/java/hardmode/Solutions.java
@@ -1003,4 +1003,42 @@ public class Solutions {
         else
             return getkth(A, aStart, B, bStart + k / 2, k - k / 2);// Check: bRight + aLeft
     }
+
+    /**
+     * no.15 https://leetcode.com/problems/3sum/
+     * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int low = i + 1;
+            int high = nums.length - 1;
+            int sum = -nums[i];
+            while (low < high) {
+                if (nums[low] + nums[high] == sum) {
+                    res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    while (low < high && nums[low] == nums[low + 1]) low++;
+                    low++;
+                    while (low < high && nums[high] == nums[high - 1]) high--;
+                    high--;
+                } else if (nums[low] + nums[high] > sum) {
+                    while (low < high && nums[high] == nums[high - 1]) high--;
+                    high--;
+                } else {
+                    while (low < high && nums[low] == nums[low + 1]) low++;
+                    low++;
+                }
+
+            }
+        }
+        return res;
+    }
 }
