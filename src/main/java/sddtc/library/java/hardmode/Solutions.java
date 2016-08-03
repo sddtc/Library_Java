5836aae37921d6174215bb9a837c47939d2d25ff
sddtc <changhbaga@gmail.com>
2016-08-03 12:39:24 +0800

    no.33

--- src/main/java/sddtc/library/java/hardmode/Solutions.java
+++ src/main/java/sddtc/library/java/hardmode/Solutions.java
@@ -1352,4 +1352,38 @@ public class Solutions {
 
         return result;
     }
+
+    /**
+     * no.33 https://leetcode.com/problems/search-in-rotated-sorted-array/
+     * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        if (null == nums || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[start] <= nums[mid]) {
+                if (target < nums[mid] && target >= nums[start])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            if (nums[mid] <= nums[end]) {
+                if (target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
